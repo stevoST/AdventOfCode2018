7662ae4f8014bb54c906ec5db60ac3edeab69d23
stevoST <stefan.starosta@gmail.com>
2018-12-21 18:06:14 +0100

    power grid solving

--- src/Day11PowerGrid.java
+++ src/Day11PowerGrid.java
@@ -8,38 +8,43 @@ public class Day11PowerGrid {
         coordinateX--;
         coordinateY--;
 
-        int powerLevel=0;
-        int totalPower=0;
-        int maximumPower=0;
+        int powerLevel = 0;
+        int totalPower = 0;
+        int maximumPower = 0;
         int[][] powerGrid = new int[300][300];
 
-        for(int i=0;i<300;i++){
-            for(int j=0;j<300;j++) {
+        for (int i = 0; i < 300; i++) {
+            for (int j = 0; j < 300; j++) {
                 int rackID;
                 rackID = i + 11;
-                powerLevel = rackID*(j+1);
+                powerLevel = rackID * (j + 1);
                 powerLevel += serialNumber;
-                powerLevel = powerLevel*rackID;
-                powerLevel = (powerLevel/100)%10;
+                powerLevel = powerLevel * rackID;
+                powerLevel = (powerLevel / 100) % 10;
 //                if(i==2 && j==4){System.out.println(powerLevel);}
-                powerGrid[i][j] = powerLevel-5;
+                powerGrid[i][j] = powerLevel - 5;
 
             }
         }
 
-        for(int x=0;x<297;x++) {
+        for (int x = 0; x < 297; x++) {
             for (int y = 0; y < 297; y++) {
 
                 for (int j = 0; j < 3; j++) {
+                    System.out.println("");
                     for (int i = 0; i < 3; i++) {
 //                        if(x==299 && y==299){System.out.println(powerGrid[coordinateX + i][coordinateY + j]);}
-//                        System.out.print(powerGrid[coordinateX + i][coordinateY + j] + " ");
+                        System.out.print(powerGrid[x + i][y + j] + " ");
                         totalPower += powerGrid[x + i][y + j];
                     }
-//                System.out.println("");
+
+                    System.out.println("");
                 }
-            if (totalPower>maximumPower) {maximumPower = totalPower;}
-                System.out.println(totalPower);
+                System.out.print("-------");
+                if (totalPower > maximumPower) {
+                    maximumPower = totalPower;
+                }
+//                System.out.println(totalPower);
 
             }
         }
